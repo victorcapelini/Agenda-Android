7c2522b390cd90015cda9c142496f691c15be13c
Victor <victorcapelini@hotmail.com>
2018-08-22 10:26:56 -0300

    Inicio do projeto. Criada primeira activity para exibir listagem. Criado primeiro Formulario para cadastro de pessoas.
    Signed-off-by: Victor <victorcapelini@hotmail.com>

---
+++ app/src/main/java/com/example/suporte7/agenda/ListaAlunosActivity.java
@@ -0,0 +1,22 @@
+package com.example.suporte7.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+
+        String[] alunos = {"Victor", "Jéssica", "Luna"};
+        ListView listaAlunos = findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+
+    }
+}
