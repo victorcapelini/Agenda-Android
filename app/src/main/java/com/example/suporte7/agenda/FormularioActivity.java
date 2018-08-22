1c72e858ee0494bfa2b34ed58e25c9d7f9000c7b
Victor <victorcapelini@hotmail.com>
2018-08-22 16:37:00 -0300

    Criado novo botão na lista que abre o formulario de cadastro.
    Signed-off-by: Victor <victorcapelini@hotmail.com>

--- app/src/main/java/com/example/suporte7/agenda/FormularioActivity.java
+++ app/src/main/java/com/example/suporte7/agenda/FormularioActivity.java
@@ -18,6 +18,7 @@ public class FormularioActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(FormularioActivity.this, "Botão clicado!",Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
 
--- app/src/main/java/com/example/suporte7/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/example/suporte7/agenda/ListaAlunosActivity.java
@@ -1,8 +1,11 @@
 package com.example.suporte7.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
@@ -13,10 +16,19 @@ public class ListaAlunosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_alunos);
 
 
-        String[] alunos = {"Victor", "Jéssica", "Luna"};
+        String[] alunos = {"Victor", "Jéssica", "Luna", "Victor", "Jéssica", "Luna", "Victor", "Jéssica", "Luna", "Victor", "Jéssica", "Luna"};
         ListView listaAlunos = findViewById(R.id.lista_alunos);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
+
+        Button botaoNovoAluno = findViewById(R.id.lista_novo_aluno);
+        botaoNovoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent vaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(vaiProFormulario);
+            }
+        });
 
     }
 }
