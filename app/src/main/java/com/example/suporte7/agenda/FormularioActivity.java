d942e0bbf99ec779f86cc649e55f2b7c65ae0cdc
Victor <victorcapelini@hotmail.com>
2018-08-22 17:28:23 -0300

    Adicionado botão de salvar ao menu superior, removendo da tela principal.
    Signed-off-by: Victor <victorcapelini@hotmail.com>

--- app/src/main/java/com/example/suporte7/agenda/FormularioActivity.java
+++ app/src/main/java/com/example/suporte7/agenda/FormularioActivity.java
@@ -2,6 +2,9 @@ package com.example.suporte7.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,17 +15,25 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+    }
 
-        Button botaoSalvar = findViewById(R.id.formulario_salvar);
-        botaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(FormularioActivity.this, "Botão clicado!",Toast.LENGTH_SHORT).show();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
+                Toast.makeText(FormularioActivity.this, "Aluno Salvo! (ou não, muahaha!)", Toast.LENGTH_SHORT).show();
                 finish();
-            }
-        });
-
-
-
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
